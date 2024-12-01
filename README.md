e5193c06acbde500cffe386ed59ce163d1921333
Francisco Javier Sánchez Chapado <fjsanchez.iabd@gmail.com>
2024-12-01 21:33:46 +0100

    Primer commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hola Mundo
+Este repositorio contiene un sencillo programa en Python que imprime "Hola Mundo".  
+Es parte de la tarea para aprender a usar GitHub.
---
+++ holamundo.py
@@ -0,0 +1,6 @@
+# Tarea para PIA01. Apartado 1.
+# 
+# Muestra en la consola el texto "Hola Mundo".
+
+# Se muestra "Hola Mundo en texto en la consola"
+print("Hola Mundo")
